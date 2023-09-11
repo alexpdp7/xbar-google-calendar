4f6624305d4b466c059c7a8c64f9c1c022aa3956
alex <alex@pdp7.net>
2023-09-11 18:38:02 +0200

    feat(order): improve ordering

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import datetime
 import os.path
+import re
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,30 +13,52 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 def event_to_order_and_item(event):
-    order = None
-    if event["start"].get("dateTime"):
-        start = datetime.datetime.strptime(event["start"]["dateTime"], "%Y-%m-%dT%H:%M:%S%z")
-        if start.date() == datetime.date.today():
-            start_str = start.strftime("%H:%M")
-            order = (0, start)
-        else:
-            start_str = start.strftime("%Y-%m-%d %H:%M")
-            order = (3, start)
-    else:
-        start = datetime.datetime.strptime(event["start"]["date"], "%Y-%m-%d")
-        if start.date() == datetime.date.today():
-            start_str = "Today"
-            order = (1, start)
-        else:
-            start_str = start.strftime("%Y-%m-%d")
-            order = (2, start)
-
-    assert order
-
     google_meet_urls = [ep["uri"] for ep in event.get("conferenceData", {}).get("entryPoints", []) if ep["uri"].startswith("https://meet.google.com")]
     google_meet_url = google_meet_urls[0] if len(google_meet_urls) == 1 else None
-    actions = f"| href={google_meet_url}" if google_meet_url else ""
-    return order, " ".join([start_str, event['summary'], actions])
+
+    bluejeans_matches = re.search(r"https://primetime.bluejeans.com/a2m/live-event/\w{8,}", event.get("description", ""))
+    bluejeans_url = bluejeans_matches.group() if bluejeans_matches else None
+
+    url = google_meet_url or bluejeans_url
+
+    created_by_me = event.get("creator", {}).get("self", False)
+
+    my_response_status = [a["responseStatus"] for a in event.get("attendees", []) if a.get("self")]
+    accepted = my_response_status == ["accepted"]
+
+    if event["start"].get("dateTime"):
+        has_time = True
+        start = datetime.datetime.strptime(event["start"]["dateTime"], "%Y-%m-%dT%H:%M:%S%z")
+        if start.date() == datetime.date.today():
+            today = True
+        else:
+            today = False
+    else:
+        has_time = False
+        start = datetime.datetime.strptime(event["start"]["date"], "%Y-%m-%d")
+        if start.date() == datetime.date.today():
+            today = True
+        else:
+            today = False
+
+    time_str = (
+        start.strftime("%H:%M") if today and has_time else
+        start.strftime("%Y-%m-%d %H:%M") if not today and has_time else
+        "Today" if today and not has_time else
+        start.strftime("%Y-%m-%d")
+    )
+
+    order_number = (
+        0 if today and accepted else
+        1 if today and has_time else
+        2 if today else
+        3
+    )
+
+    order = (order_number, abs(start.astimezone() - datetime.datetime.now().astimezone()))
+
+    actions = f"| href={url}" if url else ""
+    return order, " ".join([time_str, event['summary'], actions])
 
 
 def main():
@@ -75,11 +98,13 @@ def main():
             return
 
         order_and_items = [event_to_order_and_item(e) for e in events]
+        sorted_order_and_items = sorted(order_and_items)
 
-        for i, (_order, item) in enumerate(sorted(order_and_items)):
+        print(sorted_order_and_items[0][1])
+        print("---")
+
+        for _order, item in sorted_order_and_items:
             print(item)
-            if i == 0:
-                print("---")
         print("Refresh | refresh=true")
 
     except HttpError as error:
